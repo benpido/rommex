36c6df2dbc7539ee8fac478e36d70a05d236494d
benpido <benpido@gmail.com>
2025-06-05 13:22:15 -0400

    Eliminar duplicado

--- rommex/settings.py
+++ rommex/settings.py
@@ -39,21 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'flights',
 ]
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'flights' / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
